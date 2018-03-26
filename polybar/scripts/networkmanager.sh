a0ff80a54032a4b810c6abc2fabdfbda8cdae853
IceS2 <pjt1991@gmail.com>
2018-03-26 08:46:18 -0300

    Updated i3 scrot config and polybar network manager script

--- polybar/scripts/networkmanager.sh
+++ polybar/scripts/networkmanager.sh
@@ -3,12 +3,14 @@
 connected_interface=$(nmcli d | grep '\bconnected' | awk '{print $1}')
 
 if [[ $connected_interface = "wlp2s0" ]]; then
-	signal=$(nmcli d wifi | grep '*' | awk '{print $8}')
+	signal=$(nmcli d wifi | grep '*' | awk '{print $7}')
 	echo "%{F#08A1EC}%{F-} $signal%"
 elif [[ $connected_interface = "enp3s0" ]]; then
 	echo "%{F#08A1EC}%{F-}"
 elif [[ $connected_interface = "" ]]; then
 	echo "%{F#08A1EC}%{F-}"
+elif [[ $(echo "$connected_interface" | wc -l) == 2 ]]; then
+	echo "%{F#08A1EC}VPN%{F-}"
 else
 	echo "Unknown Interface"
 fi
