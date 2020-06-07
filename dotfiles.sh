6da4c332786233c2561a379bbae2fb0bae36537f
IceS2 <pjt1991@gmail.com>
2020-06-07 14:50:56 -0300

    Fix symlink creation

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,13 +5,13 @@
 FOLDERS=(dunst i3 kitty picom polybar ranger rofi wallpaper xres)
 
 for folder in ${FOLDERS[@]}; do
-  ln -s $HOME/.dotfiles/$folder $HOME/.config/$folder
+  ln -sn $HOME/.dotfiles/$folder $HOME/.config/$folder
 done
 
-ln -s $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sn $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
 
 HOME_FILES=(.zshrc .zshenv)
 
 for home_file in ${HOME_FILES[@]}; do
-  ln -s $HOME/.dotfiles/$home_file $HOME/$home_file
+  ln -sn $HOME/.dotfiles/$home_file $HOME/$home_file
 done
