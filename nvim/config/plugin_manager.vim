82336c86ad4ed9e85bf65f2a9142b5e86b76e174
IceS2 <pjt1991@gmail.com>
2020-09-10 10:59:33 -0300

    Update

--- nvim/config/plugin_manager.vim
+++ nvim/config/plugin_manager.vim
@@ -30,6 +30,7 @@ Plug 'kana/vim-textobj-entire'
 Plug 'kana/vim-textobj-line'
 Plug 'kana/vim-textobj-indent'
 Plug 'christoomey/vim-system-copy'
+Plug 'christoomey/vim-sort-motion'
 " python --------------------------------------------------
 Plug 'numirias/semshi', {'do': ':UpdateRemotePlugins'}
 " javascript ----------------------------------------------
--- scripts/ssh_profile.sh
+++ scripts/ssh_profile.sh
@@ -4,8 +4,8 @@ SSH_DIR="$HOME/.ssh"
 profile=$1
 
 if [ "$profile" = "personal" ]; then
-  cp $SSH_DIR/personal $SSH_DIR/id_rsa
-  cp $SSH_DIR/personal.pub $SSH_DIR/id_rsa.pub
+  cp $SSH_DIR/ices2 $SSH_DIR/id_rsa
+  cp $SSH_DIR/ices2.pub $SSH_DIR/id_rsa.pub
   export SSH_PROFILE="IceS2"
 elif [ "$profile" = "ifood" ]; then
   cp $SSH_DIR/ifood $SSH_DIR/id_rsa
